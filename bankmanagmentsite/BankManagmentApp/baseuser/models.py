acf8c50d98f762d2fba508a892bce4670519d9a6
ggmasterqwe <navid.fida@yahoo.com>
2020-10-18 19:04:30 +0330

    feat: add base user

--- bankmanagmentsite/BankManagmentApp/baseuser/models.py
+++ bankmanagmentsite/BankManagmentApp/baseuser/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers.user_manager import MainUserManager
+from root.configs.models_settings import *
+from common.constants import GenderType, UserType
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+class MainUser(AbstractBaseUser):
+    objects = MainUserManager()
+    first_name = models.CharField(max_length=MAX_FIRST_NAME, null=True, blank=True)
+    last_name = models.CharField(max_length=MAX_LAST_NAME,null=True, blank=True)
+    nationalid = models.CharField(max_length=MAX_NATIONALID, unique=True,)
+    phone_number= models.CharField(max_length=MAX_PHONE_NUMBER,unique=True)
+    password = models.CharField(max_length=100)
+    
+    birth_date = models.DateField(null=True,blank=True)
+    gender = models.IntegerField(choices=GenderType.CHOICES, default=GenderType.male)
+    user_type = models.IntegerField(choices=UserType.CHOICES, default=UserType.bank_admin)
+ 
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['nationalid', 'password']
+
+    def save(self, *args, **kwargs):
+        from BankManagementApp.models import  BranchAdmin
+        if not self.id:
+        ## set user_type
+            if isinstance(self, BranchAdmin):
+                self.user_type=UserType.branch_admin
+            
+            else:
+                self.is_admin=True
+                self.user_type=UserType.bank_admin
+
+        ## check to see if password is hashed
+        if not "sha256" in self.password:
+            self.set_password(self.password)
+
+        return super(MainUser, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.phone_number
+
+   
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
