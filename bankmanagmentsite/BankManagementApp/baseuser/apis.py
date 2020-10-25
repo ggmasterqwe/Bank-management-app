bd8b6f341b98e43736365c8f6eebc771bc9f967b
ggmasterqwe <navid.fida@yahoo.com>
2020-10-25 17:24:00 +0330

    fix: remove permission for register_status

--- bankmanagmentsite/BankManagementApp/baseuser/apis.py
+++ bankmanagmentsite/BankManagementApp/baseuser/apis.py
@@ -9,7 +9,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
-from common.permissions import IsRegisterConfirmed
+
 # Create your views here.
 
 class LoginUserAPI(ObtainAuthToken):
